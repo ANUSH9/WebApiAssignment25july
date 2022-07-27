b3478b0d05d07b39c29238d0902afdff141da9fd
ANUSH9 <loginanushgupta@gmail.com>
2022-07-27 10:46:40 +0530

    Add project files.

---
+++ WebApi3/Controllers/IDController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+namespace WebApi3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IDController : ControllerBase
+    {
+        Dictionary<int, (string, string)> obj = new Dictionary<int, (string, string)>()
+        {
+
+                {1,("delhi","local silversmiths")},
+                {2,("UP","local silversmiths")},
+                {3,("MP","local silversmiths")},
+                {4,("UK","local silversmiths")},
+                {5,("delhi","local silversmiths")},
+
+
+        };
+        [HttpGet]
+        public ActionResult GetID()
+        {
+            var serialized= JsonConvert.SerializeObject(obj);
+            return Ok(serialized);
+        }
+
+    }
+}
